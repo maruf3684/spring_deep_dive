92e7a2f95d6371b8e08ddc1f4f92b04a8cb17fe7
maruf <maruf3684@gmail.com>
2024-03-13 22:04:35 +0600

    First Commit

---
+++ src/main/java/org/cyber/universal_auth/UniversalAuthApplication.java
@@ -0,0 +1,13 @@
+package org.cyber.universal_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversalAuthApplication.class, args);
+    }
+
+}
